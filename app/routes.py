4ddd0503720ae1f4d1f9edfb57c58fe7674530f5
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2021-03-25 14:33:27 +0100

    LOGIN

--- app/routes.py
+++ app/routes.py
@@ -7,17 +7,97 @@ from flask import jsonify
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    return "Hello World", 200
+    #requête pour récupérer les joueurs
+    request_sql = f'''SELECT players_id, players_pseudo 
+    FROM players'''
+
+    #on exécute la requete
+    data = sql_select(request_sql)
+
+    #on print le résultat de la requête
+    print(data)
+
+    #on parcourt le résultat
+    for player in data:
+        #on récupère l'id du joueur
+        player_id = player["players_id"]
+
+        #requête pour récupérer les chats d'un joueur
+        request_sql = f'''SELECT * FROM cats 
+        JOIN rooms ON rooms.rooms_id = cats.rooms_id 
+        WHERE rooms.players_id = {player_id}'''
+
+
+        cats = sql_select(request_sql)
+        print(f'''CHATS DU JOUEUR {player_id} : \n''')
+        print(len(cats))
+
+        #on ajoute le nombre de chats (le nombre d'objets dans la liste renvoyée par le serveur) au player actuel
+        player["cats_count"] = len(cats)
+
+    #on renvoie le résultat jsonifié
+    return jsonify(data), 200
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    return "Not implemented", 501
+    # on récupère le json envoyé par le client
+    formulaire_login = (request.get_json())
+
+    # on récupère l'email
+    email = formulaire_login["email"]
+
+    # on check si l'email existe, si oui on check password
+    sql_request = f'''SELECT * FROM players WHERE players_email = "{email}"'''
+
+    players_avec_cette_email = sql_select(sql_request)
+
+    if len(players_avec_cette_email) > 0:
+        # on récupère le password
+        password = formulaire_login["password"]
+
+        # on check si le password est correcte
+        sql_request = f'''SELECT players_id FROM players WHERE players_email = "{email}" AND players_password = "{password}"'''
+
+        id_players_avec_email_et_password = sql_select(sql_request)
+
+        if len(id_players_avec_email_et_password) > 0:
+            print(id_players_avec_email_et_password[0]["players_id"])
+            player_id = {"id" : int(id_players_avec_email_et_password[0]["players_id"])}
+            return player_id, 418
+        else:
+            return "Password Incorrect", 403
+    else:
+        return "Email non existant", 403
 
 
 @app.route('/signup', methods=['POST'])
 def sign_up():
-    return "Not implemented", 501
+    #on récupère le json envoyé par le client
+    formulaire_inscription = (request.get_json())
+
+    #on récupère l'email
+    email = formulaire_inscription["email"]
+
+    #on check si l'email existe, si oui on envoie une erreur
+    sql_request = f'''SELECT * FROM players WHERE players_email = "{email}"'''
+
+    players_avec_cette_email = sql_select(sql_request)
+
+    if len(players_avec_cette_email) > 0:
+        return "Email déjà existant", 503
+
+    #on ajoute le joueur
+    sql_request = f'''INSERT INTO players(players_pseudo, players_email, players_password)
+    VALUES("{formulaire_inscription["pseudo"]}", 
+    "{formulaire_inscription["email"]}", 
+    "{formulaire_inscription["password"]}")'''
+
+    players_id = sql_insert(sql_request)
+
+    add_room(players_id, 0, 0, formulaire_inscription["seed"])
+
+    return "OK", 200
 
 
 @app.route('/users/<int:players_id>/rooms', methods=['GET', 'POST'])
