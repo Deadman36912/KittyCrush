d41671106c77e04b0b0538c274f50d9ebf6c2100
LAPTOP-3MCADDQ7\Vianney Bartnig <vianney.bartnig@lecole-ldlc.com>
2021-03-25 11:04:23 +0100

    preparation

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\jelly\PycharmProjects\CATS_SERVER\venv"
+$env:VIRTUAL_ENV="C:\Users\Vianney Bartnig\Documents\Github\KittyCrush\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
